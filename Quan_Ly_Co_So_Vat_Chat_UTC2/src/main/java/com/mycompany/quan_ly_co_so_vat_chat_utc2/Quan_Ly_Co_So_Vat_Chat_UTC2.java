3808c19f72cec11db7cdbdc68d52cbcb29428b9c
Vo-Thien-Linh <vothienlinh2@gmail.com>
2025-03-21 22:51:23 +0700

    themvaochinh

---
+++ Quan_Ly_Co_So_Vat_Chat_UTC2/src/main/java/com/mycompany/quan_ly_co_so_vat_chat_utc2/Quan_Ly_Co_So_Vat_Chat_UTC2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quan_ly_co_so_vat_chat_utc2;
+
+/**
+ *
+ * @author DELL
+ */
+public class Quan_Ly_Co_So_Vat_Chat_UTC2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
